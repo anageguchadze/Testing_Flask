ee82e231ba132c8f615c27847690fe646b5fed43
Ana Geguchadze <anageguchadze22@gmail.com>
2025-03-01 12:56:03 +0100

    Add models Author and Book

--- app.py
+++ app.py
@@ -13,3 +13,13 @@ class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     nationality = db.Column(db.String(100), nullable=False)
+
+    books = db.relationship('Book', backref='author', cascade='all', delete_orphan=True, lazy=True)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    publication_date = db.Column(db.Date, nullable=False)
+
+    author_id = db.Column(db.Integer, db.Foreignkey('author_id'), nullable=False)
